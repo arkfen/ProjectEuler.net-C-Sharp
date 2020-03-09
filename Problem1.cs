a1f1ced104a63ae5baae29ba2a3968b7becf5ce5
Ark Fen <arkfen@gmail.com>
2020-03-09 23:34:56 +0300

    Problem #1 Simple Solution

---
+++ Problem1.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectEuler.net_C_Sharp
+{
+    static class Problem1
+    {
+        /// <summary>
+        /// Using same loop to itterate every multiple of 3 and 5 to get the summ of all of them... and subsctucting exrtra multiple of 15 (because it is a product of 3 and 5)
+        /// </summary>
+        /// <returns>Summ of all multiples of 3 and 5 up to 1000</returns>
+        public static int GetResult()
+        {
+            int sum = 0;
+            int current3 = 3;
+            int current5 = 5;
+            int current15 = 15;
+            int limit = 1000;
+            while (current3 < limit || current5 < limit)
+            {
+                // Adding multiples of 5 until last of them reaches the limit (because it does this earlier than any multiple of 3 does)
+                sum += (current5 >= limit ? 0 : current5) + current3;
+                // Subsctructing extra multiples of 15
+                sum -= current15 >= limit ? 0 : current15;
+                current3 += 3;
+                current5 += 5;
+                current15 += 15;
+            }
+            return sum;
+        }
+    }
+}
+
+
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace ProjectEuler.net_C_Sharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Problem #1 Result is: " + Problem1.GetResult());
+
+            Console.Beep();
+            Console.Read();
+        }
+    }
+}
