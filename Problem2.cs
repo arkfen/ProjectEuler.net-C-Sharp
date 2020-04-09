4b20a94aaca46b939439dbd7e4bd434032b1f4cf
Ark Fen <arkfen@gmail.com>
2020-04-09 11:57:31 +0300

    Problem 2 solution... More or less OO SOLID style ))

---
+++ Problem2.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectEuler.net_C_Sharp
+{
+    class Problem2
+    {
+        private const int _limit = 4000000;
+
+        private int _fiboFather = 2;
+        private int _fiboGrandpa = 1;
+        
+        public int GetResult()
+        {
+            int fiboSon = _fiboFather;
+            int sum = 0;
+            while(fiboSon <= _limit)
+            {
+                if(fiboSon % 2 == 0)
+                {
+                    sum += fiboSon;
+                }
+                fiboSon = GetFiboSon();
+                ShiftFiboGeneration(fiboSon);
+            }
+            return sum;
+        }
+
+        private int GetFiboSon()
+        {
+            return _fiboGrandpa + _fiboFather;
+        }
+
+        private void ShiftFiboGeneration(int fiboSon)
+        {
+            _fiboGrandpa = _fiboFather;
+            _fiboFather = fiboSon;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,10 +6,17 @@ namespace ProjectEuler.net_C_Sharp
     {
         static void Main(string[] args)
         {
+            // Problem 1
             Console.WriteLine("Problem #1 Result is: " + Problem1.GetResult());
-
             Console.Beep();
             Console.Read();
+
+            // Problem 2
+            Problem2 problem2 = new Problem2();
+            Console.WriteLine("Problem #2 Result is: " + problem2.GetResult());
+            Console.Beep();
+            Console.Read();
+
         }
     }
 }
