7429d234f135e8a88814cd04ea38cdab714285c4
Ark Fen <arkfen@gmail.com>
2020-04-14 20:36:35 +0300

    Problem 3 Solution - Simple SOLID and still working fast code ))

---
+++ Problem3.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjectEuler.net_C_Sharp
+{
+    class Problem3
+    {
+        private const long _Number = 600851475143;
+
+
+
+        public long GetResult()
+        {
+            int pairFactor = 2;
+            long factor;
+            do
+            {
+                factor = GetFactor(pairFactor);
+                pairFactor++;
+                if (factor == 0)
+                {
+                    continue;
+                }
+                else
+                {
+                    if (isPrime(factor))
+                    {
+                        break;
+                    }
+
+                }
+            }
+            while (factor != 1);
+            return factor;
+        }
+
+
+
+
+        private bool isPrime(long number)
+        {
+            bool isPrime = true;
+            for (long i = 3; i < number; i += 2)
+            {
+                if (number % i == 0)
+                {
+                    isPrime = false;
+                    break;
+                }
+            }
+            return isPrime;
+        }
+
+
+
+
+        private long GetFactor(int pairFactor)
+        {
+            if (_Number % pairFactor == 0)
+            {
+                return _Number / pairFactor;
+            }
+            else
+            {
+                return 0;
+            }
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -6,17 +6,22 @@ namespace ProjectEuler.net_C_Sharp
     {
         static void Main(string[] args)
         {
-            // Problem 1
-            Console.WriteLine("Problem #1 Result is: " + Problem1.GetResult());
+            //// Problem 1
+            //Console.WriteLine("Problem #1 Result is: " + Problem1.GetResult());
+            //Console.Beep();
+            //Console.Read();
+
+            //// Problem 2
+            //Problem2 problem2 = new Problem2();
+            //Console.WriteLine("Problem #2 Result is: " + problem2.GetResult());
+            //Console.Beep();
+            //Console.Read();
+
+            // Problem 3
+            Problem3 problem3 = new Problem3();
+            Console.WriteLine("Problem #3 Result is: " + problem3.GetResult());
             Console.Beep();
             Console.Read();
-
-            // Problem 2
-            Problem2 problem2 = new Problem2();
-            Console.WriteLine("Problem #2 Result is: " + problem2.GetResult());
-            Console.Beep();
-            Console.Read();
-
         }
     }
 }
